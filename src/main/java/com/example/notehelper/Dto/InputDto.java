4a0393e579dc25b2fba9eae7bd3a607200a311e0
PeyHunter <Nando.goat@gmail.com>
2025-11-13 09:50:45 +0100

    Working on prompt

---
+++ src/main/java/com/example/notehelper/Dto/InputDto.java
@@ -0,0 +1,12 @@
+package com.example.notehelper.dto;
+
+public class InputDto {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+}
